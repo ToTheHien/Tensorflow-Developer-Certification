e7a843db800f0220e168630cb16345a434290adf
ToTheHien <49649938+ToTheHien@users.noreply.github.com>
2021-02-19 19:35:50 +0700

    Update SunSpots.py

--- TimeSeries/SunSpots.py
+++ TimeSeries/SunSpots.py
@@ -1,1 +1,97 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+def plot_series(time, series, format="-", start=0, end=None):
+    plt.plot(time[start:end], series[start:end], format)
+    plt.xlabel("Time")
+    plt.ylabel("Value")
+    plt.grid(True)
+    
+!wget --no-check-certificate \
+    https://storage.googleapis.com/laurencemoroney-blog.appspot.com/Sunspots.csv \
+    -O /tmp/sunspots.csv
 
+import csv
+time_step = []
+SN = []
+with open('/tmp/sunspots.csv') as csvfile:
+  reader = csv.reader(csvfile, delimiter=',')
+  next(reader)
+  step=0
+  for row in reader:
+    SN.append(float(row[2]))
+    time_step.append(step)
+    step = step + 1
+series = np.array(SN)
+time = np.array(time_step)
+plt.figure(figsize=(10, 6))
+plot_series(time, series)
+
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler()
+series = scaler.fit_transform(series.reshape(-1, 1))
+print(series)
+series = series.flatten()
+series
+
+split_time = 3000
+time_train = time[:split_time]
+x_train = series[:split_time]
+time_valid = time[split_time:]
+x_valid = series[split_time:]
+window_size = 60
+batch_size = 64
+shuffle_buffer_size = 1000
+
+def windowed_dataset(series, window_size, batch_size, shuffle_buffer):
+  series = tf.expand_dims(series, axis=-1)
+  ds = tf.data.Dataset.from_tensor_slices(series)
+  ds = ds.window(window_size + 1, shift=1, drop_remainder=True)
+  ds = ds.flat_map(lambda window: window.batch(window_size + 1))
+  ds = ds.shuffle(shuffle_buffer)
+  ds = ds.map(lambda window: (window[:-1], window[1:]))
+  ds = ds.batch(batch_size).prefetch(1)
+  return ds
+
+train_set = windowed_dataset(x_train, window_size=60, batch_size=64, shuffle_buffer=shuffle_buffer_size)
+
+one = list(train_set.take(1).as_numpy_iterator())[0]
+one[0].shape
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense, Bidirectional, Conv1D
+
+model = Sequential([
+      #Conv1D(filters=64, kernel_size=5, strides=1, padding='same', activation='relu', input_shape=[None, 1]),              
+      Bidirectional(LSTM(64, return_sequences=True)),
+      Bidirectional(LSTM(64, return_sequences=True)),
+      Dense(32, activation='relu'),
+      Dense(16, activation='relu'),
+      Dense(1)
+])
+
+tf.random.set_seed(0)
+np.random.seed(0)
+
+model.compile(loss=tf.keras.losses.Huber(), optimizer='adam', metrics=['mae'])
+history = model.fit(train_set, epochs=100)
+
+def model_forecast(model, series, window_size):
+  ds = tf.data.Dataset.from_tensor_slices(series)
+  ds = ds.window(window_size, shift=1 , drop_remainder=True)
+  ds = ds.flat_map(lambda w: w.batch(window_size))
+  ds = ds.batch(32).prefetch(1)
+  forecast = model.predict(ds)
+  return forecast
+
+rnn_forecast = model_forecast(model, series[..., np.newaxis], window_size)
+rnn_forecast = rnn_forecast[split_time - window_size:-1, -1, 0]
+tf.keras.metrics.mean_absolute_error(x_valid, rnn_forecast).numpy()
+
+with plt.style.context('ggplot'):
+    plt.figure(figsize=(16, 8))
+    plot_series(time_valid, x_valid)
+    plot_series(time_valid, rnn_forecast)
+    labels = ['Real', 'Predicted']
+    plt.legend(labels)
+    plt.show()
