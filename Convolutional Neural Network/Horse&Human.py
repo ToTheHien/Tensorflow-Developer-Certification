fcf459901eccc36afd7b2d95bf4188eac87f3c5e
ToTheHien <49649938+ToTheHien@users.noreply.github.com>
2021-02-19 15:21:54 +0700

    Update Horse&Human.py

--- Convolutional Neural Network/Horse&Human.py
+++ Convolutional Neural Network/Horse&Human.py
@@ -1,1 +1,95 @@
+!wget --no-check-certificate \
+    https://storage.googleapis.com/laurencemoroney-blog.appspot.com/horse-or-human.zip \
+    -O /tmp/horse-or-human.zip
+  
+!wget --no-check-certificate \
+    https://storage.googleapis.com/laurencemoroney-blog.appspot.com/validation-horse-or-human.zip \
+    -O /tmp/validation-horse-or-human.zip
+  
+import os
+import zipfile
 
+local_zip = '/tmp/horse-or-human.zip'
+zip_ref = zipfile.ZipFile(local_zip, 'r')
+zip_ref.extractall('/tmp/horse-or-human')
+local_zip = '/tmp/validation-horse-or-human.zip'
+zip_ref = zipfile.ZipFile(local_zip, 'r')
+zip_ref.extractall('/tmp/validation-horse-or-human')
+zip_ref.close()
+
+# Directory with our training horse pictures
+train_horse_dir = os.path.join('/tmp/horse-or-human/horses')
+
+# Directory with our training human pictures
+train_human_dir = os.path.join('/tmp/horse-or-human/humans')
+
+# Directory with our training horse pictures
+validation_horse_dir = os.path.join('/tmp/validation-horse-or-human/horses')
+
+# Directory with our training human pictures
+validation_human_dir = os.path.join('/tmp/validation-horse-or-human/humans')
+
+train_horse_names = os.listdir(train_horse_dir)
+train_human_names = os.listdir(train_human_dir)
+
+validation_horse_names = os.listdir(validation_horse_dir)
+validation_human_names = os.listdir(validation_human_dir)
+
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPool2D, Flatten, Dense
+
+model = Sequential([
+    Conv2D(16, (3,3), activation='relu', input_shape=(300, 300, 3)),
+    MaxPool2D(2, 2),
+    Conv2D(32, (3,3), activation='relu'),
+    MaxPool2D(2,2),
+    Conv2D(64, (3,3), activation='relu'),
+    MaxPool2D(2,2),
+    Conv2D(64, (3,3), activation='relu'),
+    MaxPool2D(2,2),
+    Conv2D(64, (3,3), activation='relu'),
+    MaxPool2D(2,2),
+    Flatten(),
+    Dense(512, activation='relu'),
+    # 0: horse | 1: human
+    Dense(1, activation='sigmoid')
+])
+
+model.summary()
+
+from tensorflow.keras.optimizers import RMSprop
+
+model.compile( 
+    loss = 'binary_crossentropy',
+    optimizer = RMSprop(lr=0.0001),
+    metrics = ['accuracy']
+)
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(rescale = 1/255)
+validation_datagen = ImageDataGenerator(rescale = 1/255)
+
+
+train_generator = train_datagen.flow_from_directory(
+        '/tmp/horse-or-human',
+        target_size = (300, 300),
+        batch_size = 128,
+        class_mode = 'binary'
+        )
+
+validation_generator = validation_datagen.flow_from_directory(
+        '/tmp/validation-horse-or-human',
+        target_size = (300, 300),
+        batch_size = 32,
+        class_mode = 'binary'
+        )
+
+history = model.fit(
+      train_generator,
+      epochs = 15,
+      steps_per_epoch = 8,
+      verbose=1,
+      validation_data = validation_generator
+      )
