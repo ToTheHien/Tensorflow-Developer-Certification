cfe5c0c9c2932c28253d48a84e4eed6e95fd7f1a
ToTheHien <49649938+ToTheHien@users.noreply.github.com>
2021-02-19 16:28:36 +0700

    Update RockPaperScissor (MobileNet).py

--- Convolutional Neural Network/RockPaperScissor (MobileNet).py
+++ Convolutional Neural Network/RockPaperScissor (MobileNet).py
@@ -1,1 +1,77 @@
+!wget --no-check-certificate \
+    https://storage.googleapis.com/laurencemoroney-blog.appspot.com/rps.zip \
+    -O /tmp/rps.zip
+  
+!wget --no-check-certificate \
+    https://storage.googleapis.com/laurencemoroney-blog.appspot.com/rps-test-set.zip \
+    -O /tmp/rps-test-set.zip
+  
+import os
+import zipfile
 
+local_zip = '/tmp/rps.zip'
+zip_ref = zipfile.ZipFile(local_zip, 'r')
+zip_ref.extractall('/tmp/')
+zip_ref.close()
+
+local_zip = '/tmp/rps-test-set.zip'
+zip_ref = zipfile.ZipFile(local_zip, 'r')
+zip_ref.extractall('/tmp/')
+zip_ref.close()
+
+rock_dir = os.path.join('/tmp/rps/rock')
+paper_dir = os.path.join('/tmp/rps/paper')
+scissors_dir = os.path.join('/tmp/rps/scissors')
+
+rock_files = os.listdir(rock_dir)
+paper_files = os.listdir(paper_dir)
+scissors_files = os.listdir(scissors_dir)
+
+import tensorflow as tf
+import keras_preprocessing
+from keras_preprocessing import image
+from keras_preprocessing.image import ImageDataGenerator
+
+TRAINING_DIR = "/tmp/rps/"
+training_datagen = ImageDataGenerator(
+      rescale = 1./255,
+	  rotation_range=40,
+      width_shift_range=0.2,
+      height_shift_range=0.2,
+      shear_range=0.2,
+      zoom_range=0.2,
+      horizontal_flip=True,
+      fill_mode='nearest')
+
+VALIDATION_DIR = "/tmp/rps-test-set/"
+validation_datagen = ImageDataGenerator(rescale = 1./255)
+
+train_generator = training_datagen.flow_from_directory(
+	TRAINING_DIR,
+	target_size=(224,224),
+  batch_size=64,
+	class_mode='categorical'
+)
+
+validation_generator = validation_datagen.flow_from_directory(
+	VALIDATION_DIR,
+	target_size=(224,224),
+  batch_size=64,
+	class_mode='categorical'
+)
+
+import tensorflow_hub as hub
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, InputLayer
+
+mobilenet_url = "https://tfhub.dev/google/tf2-preview/mobilenet_v2/feature_vector/4" 
+
+mobile = Sequential()
+mobile.add(InputLayer(input_shape=(224,224,3)))
+mobile.add(hub.KerasLayer(mobilenet_url, trainable=False))
+mobile.add(Dense(3, activation="softmax"))
+mobile.summary()
+
+mobile.compile(loss = 'categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+
+history = mobile.fit_generator(train_generator, epochs=4, validation_data = validation_generator, verbose = 1)
